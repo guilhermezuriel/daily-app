849e218bad024b6a9be8f4381d084bb2e3405020
Guilherme Zuriel <guizuriel.dev@gmail.com>
2024-03-16 17:10:30 -0300

    Criação do controller User

---
+++ daily-app-back-end/src/controller/UserController.ts
@@ -0,0 +1,22 @@
+import { Request, Response } from "express";
+import {z} from 'zod'
+
+const UserController = { 
+  async createUserController(req:Request, res: Response){
+    try{
+      const createUserBodySchema = z.object({
+        name: z.string(),
+        email: z.string().refine((email)=>/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g.test(email?? ""), 'Invalid email'),
+        password: z.string().refine((password)=>/^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$"/g.test(password??''), 'Password must have minimum eight characters, at least one letter,one number and special character'),
+
+      })
+      const user = req.body;
+
+    }catch(err){
+      console.log('createUserContoller >>>', err)
+    }
+  },
+
+}
+
+export default UserController
