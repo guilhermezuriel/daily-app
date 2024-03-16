8f5ad2cdfc8d76f6f718069789a05d35ec9c1a6d
Guilherme Zuriel <guizuriel.dev@gmail.com>
2024-03-15 23:25:48 -0300

    Configuração da extração de dados do process env

---
+++ daily-app-back-end/src/env/index.ts
@@ -0,0 +1,28 @@
+import { config } from "dotenv";
+import {z} from 'zod';
+
+if(process.env.NODE_ENV === 'test'){
+  config({path:'.env.test'})
+}else{
+  config()
+}
+
+const envSchema = z.object({
+    NODE_ENV: z.enum(['development', 'test', 'production']).default('production'),
+    DATABASE_URL: z.string(),
+    PORT: z.number().default(3000)
+})
+
+
+const _env = envSchema.safeParse(process.env)
+
+console.log(_env);
+
+if(_env.success == false){
+  console.log('Invalid Enviroment Variables', _env.error.format());
+  throw new Error('Invalid Enviroment variables')
+}
+
+const env = _env.data;
+
+export default env;
