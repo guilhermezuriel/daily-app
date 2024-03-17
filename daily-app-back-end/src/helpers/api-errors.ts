b92e2f3390d30c3f333dd45aa6a61658872396f6
Guilherme Zuriel <guizuriel.dev@gmail.com>
2024-03-17 12:13:21 -0300

    Criação de uma classe helper de erros

---
+++ daily-app-back-end/src/helpers/api-errors.ts
@@ -0,0 +1,8 @@
+export class Errors{
+  message:string
+  statusCode:number
+  constructor(message:string, statusCode:number){
+    this.message = message;
+    this.statusCode = statusCode;
+  }
+}
