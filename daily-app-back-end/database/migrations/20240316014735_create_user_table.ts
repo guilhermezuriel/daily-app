8d1c789a0d9d581668e8ec8b87e5596e070de62d
Guilherme Zuriel <guizuriel.dev@gmail.com>
2024-03-19 17:49:29 -0300

    Ajustes das migrations

--- daily-app-back-end/database/migrations/20240316014735_create_user_table.ts
+++ daily-app-back-end/database/migrations/20240316014735_create_user_table.ts
@@ -3,7 +3,7 @@ import type { Knex } from "knex";
 
 export async function up(knex: Knex): Promise<void> {
   await knex.schema.createTable('users', (table)=>{
-    table.uuid('id').primary()
+    table.uuid('id').primary().index()
     table.text('name').notNullable()
     table.text('email').notNullable().unique()
     table.text('password').notNullable()
--- daily-app-back-end/database/migrations/20240316132904_create_refs.ts
+++ daily-app-back-end/database/migrations/20240316132904_create_refs.ts
@@ -3,7 +3,7 @@ import type { Knex } from "knex";
 
 export async function up(knex: Knex): Promise<void> {
   knex.schema.createTable('refs',(table)=>{
-    table.uuid('id').primary()
+    table.uuid('id').primary().index()
     table.uuid('user_id') 
     table.text('name')
     table.enu('type', ['breakfast','lunch','dinner','snack'])
