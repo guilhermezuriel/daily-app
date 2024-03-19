efe9ceea2232cae4192b6c8b2a9c90901ad3b4e6
Guilherme Zuriel <guizuriel.dev@gmail.com>
2024-03-19 09:28:14 -0300

    Criação do Controller de Login do user

---
+++ daily-app-back-end/src/controller/LoginController.ts
@@ -0,0 +1,28 @@
+import { Request, Response } from "express";
+import { z } from 'zod'
+import { kknex } from "../database";
+import { BadRequestError } from "../helpers/api-errors";
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+
+export const LoginController = {
+  async loginUserController(req: Request, res: Response) {
+    try {
+      const { email, password } = req.body;
+
+      const [user]= await kknex('users').where('email', email).select('*');
+      if (!user) return new BadRequestError('Email ou senha inválidos');
+
+      const verifyPassword = await bcrypt.compare(password, user.password);
+      if (!verifyPassword) return new BadRequestError('Email ou senha inválidos')
+      const token = jwt.sign({ id: user.id }, process.env.JWT_PASS ?? '', { expiresIn: 60 * 60 * 24 * 7 });//7 days
+      //CODE (STORE TOKEN ON LOCALSTORAGE OR COOKIES)
+      const { password: _, ...userLogin } = user;
+      return res.json({ user: userLogin });
+    } catch (err) {
+      console.log('loginUserController >>>', err)
+    }
+  },
+  
+
+}
