38bbd2c814c0736b450b7ce124a60017c3e36c6a
Guilherme Zuriel <guizuriel.dev@gmail.com>
2024-03-19 11:41:08 -0300

    Criação da rota DELETE

--- daily-app-back-end/src/routes/CreateUserRouter.ts
+++ daily-app-back-end/src/routes/CreateUserRouter.ts
@@ -9,6 +9,7 @@ const route = Router();
 route.get('/', UserController.listAllUsersController);
 route.post('/create', UserController.createUserController);
 route.get('/profile', verifyJWT, UserController.getUserProfileController);
+route.delete('/delete', verifyJWT, UserController.deleteUserController);
 
 
 export default route;
