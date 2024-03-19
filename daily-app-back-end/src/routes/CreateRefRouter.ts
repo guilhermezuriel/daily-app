4a410e0bfd81ead97a59268502d94a7dd684e348
Guilherme Zuriel <guizuriel.dev@gmail.com>
2024-03-19 11:25:30 -0300

    Rotas refs

--- daily-app-back-end/src/routes/CreateRefRouter.ts
+++ daily-app-back-end/src/routes/CreateRefRouter.ts
@@ -1,9 +1,12 @@
 import { Router, Request, Response, request } from "express";
 
 import RefController from '../controller/RefController'
+import { verifyJWT } from "../middleware/verifyJWT";
 
 const route = Router();
 
 route.post('/', RefController.createRefController);
+route.get('/:id', RefController.getRefController)
+route.put('/:id/edit', RefController.updateRefController)
 
 export default route;
--- daily-app-back-end/src/routes/index.ts
+++ daily-app-back-end/src/routes/index.ts
@@ -1,11 +1,14 @@
 import CreateUserRouter from "./CreateUserRouter";
 import CreateLoginRouter from "./CreateLoginRouter";
+import CreateRefRouter from "./CreateRefRouter";
+import { verifyJWT } from "../middleware/verifyJWT";
 import { Router} from "express";
 
 const routes = Router();
 
 routes.use('/login', CreateLoginRouter)
 routes.use('/user',CreateUserRouter)
+routes.use('/ref',verifyJWT, CreateRefRouter )
 
 
 export default routes
